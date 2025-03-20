015135577f90819e8ca5749855e16aed299f7c30
shakhozumarov <umarovshakhzod04@gmail.com>
2025-03-20 09:17:19 -0400

    r

--- components/LanguageSwitcher/index.tsx
+++ components/LanguageSwitcher/index.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { useState, useEffect, useRef } from 'react';
 import { useLocale } from 'next-intl';
-import { Link } from '../../i18n/routing';
+import { Link, usePathname } from '../../i18n/routing';
 import styles from './languageSwitcher.module.scss';
 
 const LanguageSwitcher = () => {
   const locale = useLocale(); // Get the current locale
+  const pathname = usePathname(); // Get the current path
   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State to control dropdown visibility
   const dropdownRef = useRef<HTMLDivElement>(null);
-
+  
   // Function to toggle the dropdown
   const toggleDropdown = () => setIsDropdownOpen((prevState) => !prevState);
-
+  
   // Close dropdown when clicking outside
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -19,35 +20,35 @@ const LanguageSwitcher = () => {
         setIsDropdownOpen(false);
       }
     };
-
+    
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
-
+  
   return (
     <div className={styles.languageSwitcher} ref={dropdownRef}>
       {/* Button to toggle the dropdown */}
       <button 
-        className={`${styles.switchBtn} ${isDropdownOpen ? styles.active : ''}`} 
+        className={`${styles.switchBtn} ${isDropdownOpen ? styles.active : ''}`}
         onClick={toggleDropdown}
         aria-expanded={isDropdownOpen ? 'true' : 'false'} // Correct aria-expanded value
       >
         {locale === 'uz' ? 'UZ' : locale === 'en' ? 'EN' : 'RU'}
         <span className={styles.arrow}></span>
       </button>
-
+      
       {/* Dropdown menu */}
       <div className={`${styles.dropdownMenu} ${isDropdownOpen ? styles.open : ''}`}>
-        <Link href="/" locale={locale === 'uz' ? 'en' : locale === 'en' ? 'ru' : 'uz'} passHref>
+        <Link href={pathname} locale={locale === 'uz' ? 'en' : locale === 'en' ? 'ru' : 'uz'} passHref>
           <button className={styles.dropdownItem}>
             {locale === 'uz' ? 'EN' : locale === 'en' ? 'RU' : 'UZ'}
           </button>
         </Link>
-
+        
         {/* Second option */}
-        <Link href="/" locale={locale === 'uz' ? 'ru' : locale === 'en' ? 'uz' : 'en'} passHref>
+        <Link href={pathname} locale={locale === 'uz' ? 'ru' : locale === 'en' ? 'uz' : 'en'} passHref>
           <button className={styles.dropdownItem}>
             {locale === 'uz' ? 'RU' : locale === 'en' ? 'UZ' : 'EN'}
           </button>
